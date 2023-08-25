ba47c40255c285af040228c1d8d6b3e62c50ddcb
Madagascar123withSlash <1264284830@qq.com>
2023-08-25 17:28:45 +0800

    C sharp的取图功能

--- SuperVision/SuperVision/Form1.Designer.cs
+++ SuperVision/SuperVision/Form1.Designer.cs
@@ -28,13 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox_main = new System.Windows.Forms.PictureBox();
+            this.btn_showpic = new System.Windows.Forms.Button();
+            this.btn_picReadConnect = new System.Windows.Forms.Button();
+            this.btn_picStream = new System.Windows.Forms.Button();
+            this.btn_dataProcess = new System.Windows.Forms.Button();
+            this.btn_displayPic = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_main)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_main
+            // 
+            this.pictureBox_main.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox_main.Name = "pictureBox_main";
+            this.pictureBox_main.Size = new System.Drawing.Size(589, 452);
+            this.pictureBox_main.TabIndex = 0;
+            this.pictureBox_main.TabStop = false;
+            // 
+            // btn_showpic
+            // 
+            this.btn_showpic.Location = new System.Drawing.Point(621, 12);
+            this.btn_showpic.Name = "btn_showpic";
+            this.btn_showpic.Size = new System.Drawing.Size(154, 39);
+            this.btn_showpic.TabIndex = 1;
+            this.btn_showpic.Text = "测试";
+            this.btn_showpic.UseVisualStyleBackColor = true;
+            this.btn_showpic.Click += new System.EventHandler(this.btn_showpic_Click);
+            // 
+            // btn_picReadConnect
+            // 
+            this.btn_picReadConnect.Location = new System.Drawing.Point(621, 75);
+            this.btn_picReadConnect.Name = "btn_picReadConnect";
+            this.btn_picReadConnect.Size = new System.Drawing.Size(154, 42);
+            this.btn_picReadConnect.TabIndex = 2;
+            this.btn_picReadConnect.Text = "连接串口";
+            this.btn_picReadConnect.UseVisualStyleBackColor = true;
+            this.btn_picReadConnect.Click += new System.EventHandler(this.btn_picReadConnect_Click);
+            // 
+            // btn_picStream
+            // 
+            this.btn_picStream.Location = new System.Drawing.Point(621, 141);
+            this.btn_picStream.Name = "btn_picStream";
+            this.btn_picStream.Size = new System.Drawing.Size(154, 45);
+            this.btn_picStream.TabIndex = 3;
+            this.btn_picStream.Text = "获取数据流";
+            this.btn_picStream.UseVisualStyleBackColor = true;
+            this.btn_picStream.Click += new System.EventHandler(this.btn_picStream_Click);
+            // 
+            // btn_dataProcess
+            // 
+            this.btn_dataProcess.Location = new System.Drawing.Point(621, 204);
+            this.btn_dataProcess.Name = "btn_dataProcess";
+            this.btn_dataProcess.Size = new System.Drawing.Size(154, 45);
+            this.btn_dataProcess.TabIndex = 4;
+            this.btn_dataProcess.Text = "数据拼接";
+            this.btn_dataProcess.UseVisualStyleBackColor = true;
+            this.btn_dataProcess.Click += new System.EventHandler(this.btn_dataProcess_Click);
+            // 
+            // btn_displayPic
+            // 
+            this.btn_displayPic.Location = new System.Drawing.Point(621, 266);
+            this.btn_displayPic.Name = "btn_displayPic";
+            this.btn_displayPic.Size = new System.Drawing.Size(154, 45);
+            this.btn_displayPic.TabIndex = 5;
+            this.btn_displayPic.Text = "展示图像";
+            this.btn_displayPic.UseVisualStyleBackColor = true;
+            this.btn_displayPic.Click += new System.EventHandler(this.btn_displayPic_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_displayPic);
+            this.Controls.Add(this.btn_dataProcess);
+            this.Controls.Add(this.btn_picStream);
+            this.Controls.Add(this.btn_picReadConnect);
+            this.Controls.Add(this.btn_showpic);
+            this.Controls.Add(this.pictureBox_main);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_main)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_main;
+        private System.Windows.Forms.Button btn_showpic;
+        private System.Windows.Forms.Button btn_picReadConnect;
+        private System.Windows.Forms.Button btn_picStream;
+        private System.Windows.Forms.Button btn_dataProcess;
+        private System.Windows.Forms.Button btn_displayPic;
     }
 }
 
--- SuperVision/SuperVision/Form1.cs
+++ SuperVision/SuperVision/Form1.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using System.IO.Ports;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
 
 namespace SuperVision
 {
@@ -15,6 +20,137 @@ namespace SuperVision
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Mat img1 = new Mat("C:\\Users\\Thinkpad\\Desktop\\test.jpg");
+        private void btn_showpic_Click(object sender, EventArgs e)
+        {
+            Cv2.ImShow("123", img1);
+            Cv2.WaitKey(0);
+        }
+        private SerialPort picReadSerialPort;
+        private void btn_picReadConnect_Click(object sender, EventArgs e)
+        {
+            if (this.btn_picReadConnect.Text == "连接串口")
+            {
+                picReadSerialPort = new SerialPort();
+                picReadSerialPort.BaudRate = 115200;
+                picReadSerialPort.PortName = "COM6";
+                picReadSerialPort.StopBits = StopBits.One;
+                picReadSerialPort.DataBits = 8;
+                picReadSerialPort.Parity = Parity.None;
+                picReadSerialPort.ReadBufferSize = 1024000;
+                picReadSerialPort.ReadTimeout = 1000;
+                picReadSerialPort.RtsEnable = true;
+                picReadSerialPort.DtrEnable = true;
+                //picReadSerialPort.Handshake = Handshake.RequestToSend;
+                picReadSerialPort.DataReceived += new SerialDataReceivedEventHandler(GetRecvMsg);
+                try
+                {
+                    picReadSerialPort.Open();
+
+                    MessageBox.Show("连接成功");
+                    this.btn_picReadConnect.Text = "串口关闭";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("连接失败：" + ex.Message);
+                }
+
+            }
+            else
+            {
+                picReadSerialPort.Close();
+                MessageBox.Show("关闭成功");
+                this.btn_picReadConnect.Text = "连接串口";
+            }
+        }
+        private void send_menu_cmd(string cmd)
+        {
+            string hex_1 = "02";
+            string hex_2 = "f0";
+            string hex_3 = "03";
+            byte[] cmd_head = new byte[3];
+            cmd_head[0] = Convert.ToByte(hex_1, 16);
+            cmd_head[1] = Convert.ToByte(hex_2, 16);
+            cmd_head[2] = Convert.ToByte(hex_3, 16);
+            byte[] cmd_main = new byte[cmd.Length];
+            cmd_main = Encoding.ASCII.GetBytes(cmd);
+            byte[] cmd_combine = new byte[cmd.Length + 3];
+            cmd_head.CopyTo(cmd_combine, 0);
+            cmd_main.CopyTo(cmd_combine, 3);
+
+            picReadSerialPort.Write(cmd_combine, 0, cmd_combine.Length);
+
+
+        }
+
+        private void btn_picStream_Click(object sender, EventArgs e)
+        {
+
+
+            send_menu_cmd("040807.");
+            
+            
+        }
+        List<byte[]> streamList = new List<byte[]>();
+        System.Text.UTF8Encoding decoder = new UTF8Encoding();
+        int byteCnt = 0;
+        private void GetRecvMsg(object sender,SerialDataReceivedEventArgs e)
+        {
+
+            if (picReadSerialPort.BytesToRead > 0)
+            {
+                
+                var picValue = new byte[picReadSerialPort.BytesToRead];
+                picReadSerialPort.Read(picValue, 0, picReadSerialPort.BytesToRead);
+                byteCnt+= picValue.Length;
+                streamList.Add(picValue);
+                string strdecode = decoder.GetString(picValue);
+                if (strdecode.IndexOf("040807") > 0 )
+                {
+                    Console.WriteLine("111111");
+                }
+                //Console.WriteLine(strdecode);
+                //Console.WriteLine(strdecode.Length.ToString());
+                //Console.WriteLine("列表大小：" + streamList.Count.ToString());
+            }
+            
+        }
+        private byte[] picbuffer;
+        private void btn_dataProcess_Click(object sender, EventArgs e)
+        {
+            byte[] dataFlow = new byte[byteCnt];
+            int memberCnt =0;//list里面成员大小的累计
+            foreach (byte[] l in streamList) 
+            {
+                Buffer.BlockCopy(l, 0, dataFlow, memberCnt, l.Length);
+                memberCnt+= l.Length;
+            }
+            string flowdecode = decoder.GetString(dataFlow);
+            int at = flowdecode.IndexOf("JFIF", 0)-6;
+            int end = flowdecode.IndexOf("040807", 100);
+            picbuffer = new byte[end-at];
+            Buffer.BlockCopy(dataFlow, at, picbuffer, 0, end - at);
+            //Console.WriteLine(decoder.GetString(picbuffer));
+        }
+
+        private void btn_displayPic_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var bitmap = Image.FromStream(new MemoryStream(picbuffer));//数据流自带图像信息，直接转化。
+                this.pictureBox_main.Image = bitmap;
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            streamList.Clear();
+            byteCnt = 0;
+
         }
     }
 }
